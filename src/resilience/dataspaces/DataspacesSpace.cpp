ecd7451fab781064a21f774570c67d62a082b3a3
Zhang690683220 <zhangbo_china@live.cn>
2020-08-02 21:58:42 -0400

    set appid to hash(time+pid)

--- src/resilience/dataspaces/DataspacesSpace.cpp
+++ src/resilience/dataspaces/DataspacesSpace.cpp
@@ -23,6 +23,12 @@ namespace KokkosResilience {
     int KokkosDataspacesAccessor::initialize( const size_t size_, const std::string & filepath) {
         data_size = size_;
         file_path = filepath;
+        time_t rawtime;
+        time(&rawtime);
+        std::string time_str (ctime(&rawtime));
+        std::string pid_str = std::to_string((int)getpid());
+        std::hash<std::string> str_hash;
+        appid = str_hash(time_str+pid_str) % std::numeric_limits<int>::max();
         MPI_Comm_size( MPI_COMM_WORLD, &mpi_size );
         MPI_Comm_rank( MPI_COMM_WORLD, &mpi_rank);
         dspaces_init(mpi_size, 1, &gcomm, NULL); // TODO: How to define appid ?
