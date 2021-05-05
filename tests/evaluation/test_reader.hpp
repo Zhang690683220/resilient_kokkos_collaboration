9062901a8fa88e948a9713b6a8e361d550d54409
Zhang690683220 <zhangbo_china@live.cn>
2021-05-05 15:26:12 -0400

    debug

--- tests/evaluation/test_reader.hpp
+++ tests/evaluation/test_reader.hpp
@@ -79,6 +79,340 @@ struct kokkos_run {
 };
 
 template <class Data_t>
+struct kokkos_run<Data_t, 2, KokkosResilience::StdFileSpace> {
+static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
+                    uint64_t* src_sp, uint64_t* offset, int timesteps,
+                    int var_num, bool terminate)
+{
+    int rank, nprocs;
+    MPI_Comm_size(gcomm, &nprocs);
+    MPI_Comm_rank(gcomm, &rank);
+
+
+    using ViewHost_t    = Kokkos::View<Data_t**, Kokkos::HostSpace>;
+    using ViewStaging_t = Kokkos::View<Data_t**, KokkosResilience::StdFileSpace>;
+
+    uint64_t* off = (uint64_t*) malloc(2*sizeof(uint64_t));
+    uint64_t* lb = (uint64_t*) malloc(2*sizeof(uint64_t));
+    uint64_t* ub = (uint64_t*) malloc(2*sizeof(uint64_t));
+
+    // get the lb & ub for each rank
+    for(int i=0; i<2; i++) {
+        int tmp = rank;
+        for(int j=0; j<i; j++)
+            tmp /= np[j];
+        off[i] = tmp % np[i] * sp[i];
+        lb[i] = off[i] + offset[i];
+        ub[i] = off[i] + sp[i] - 1 + offset[i];
+    }
+
+    struct bbox local_bb;
+
+    local_bb.num_dims = 2;
+    memcpy(local_bb.lb.c, lb, 2*sizeof(uint64_t));
+    memcpy(local_bb.ub.c, ub, 2*sizeof(uint64_t));
+
+    struct bbox* src_bbox_tab = (struct bbox*) malloc(src_np[0]*src_np[1]*sizeof(struct bbox));
+    int iter[2];
+    for(iter[0]=0; iter[0]<src_np[0]; iter[0]++) {
+        for(iter[1]=0; iter[1]<src_np[1]; iter[1]++) {
+                src_bbox_tab[iter[0]*src_np[1]+iter[1]].num_dims = 2;
+                for(int d=0; d<2; d++) {
+                    src_bbox_tab[iter[0]*src_np[1]+iter[1]].lb.c[d] = iter[d]*src_sp[d];
+                    src_bbox_tab[iter[0]*src_np[1]+iter[1]].ub.c[d] = (iter[d]+1)*src_sp[d]-1;
+                }
+                std::cout<<"lb: {"<<src_bbox_tab[iter[0]*src_np[1]+iter[1]].lb.c[0]<<", "
+                        <<src_bbox_tab[iter[0]*src_np[1]+iter[1]].lb.c[1]<<"}\n"
+                        <<"ub: {"<<src_bbox_tab[iter[0]*src_np[1]+iter[1]].ub.c[0]<<", "
+                        <<src_bbox_tab[iter[0]*src_np[1]+iter[1]].ub.c[1]<<"}"<<std::endl;
+        }
+    }
+    
+
+    
+    int open_num = 0;
+    for(int i=0; i<src_np[0]*src_np[1]; i++) {
+        if(bbox_does_intersect(&local_bb, &src_bbox_tab[i])) {
+            open_num++;
+        }
+    }
+
+    std::vector<std::string> open_tab;
+
+    open_tab.resize(open_num);
+    for(int i=0; i<src_np[0]*src_np[1]; i++) {
+        if(bbox_does_intersect(&local_bb, &src_bbox_tab[i])) {
+            std::string tmp = "StagingView_2D_" + std::to_string(src_bbox_tab[i].lb.c[0]) + "_"
+                                + std::to_string(src_bbox_tab[i].lb.c[1]) + "_"
+                                + std::to_string(src_bbox_tab[i].ub.c[0]) + "_" + std::to_string(src_bbox_tab[i].ub.c[1]);
+            open_tab[i] = tmp;
+        }
+    }
+
+
+
+
+    ViewHost_t v_G("GetView", sp[0], sp[1]);
+    ViewHost_t v_tmp("TmpView", src_sp[0], src_sp[1]);
+
+
+    std::ofstream log;
+    double* avg_read = nullptr;
+    double total_avg = 0;
+
+    if(rank == 0) {
+        avg_read = (double*) malloc(sizeof(double)*timesteps);
+        log.open("test_reader.log", std::ofstream::out | std::ofstream::trunc);
+        log << "step\tread_gs" << std::endl;
+    }
+
+    for(int ts=1; ts<=timesteps; ts++) {
+
+        Timer timer_read;
+
+        timer_read.start();
+
+        for(int i=0; i<open_tab.size(); i++) {
+            std::string filename = open_tab[i] + "_t" + std::to_string(ts) + ".bin";
+
+            ViewStaging_t v_S(filename, src_sp[0], src_sp[1]);
+
+            Kokkos::deep_copy(v_tmp, v_S);
+
+            struct bbox tmp_bbox;
+
+            bbox_intersect(&local_bb, &src_bbox_tab[i], &tmp_bbox);
+            
+
+            Kokkos::parallel_for(sp[0], KOKKOS_LAMBDA(const int i0) {
+                for(int i1=0; i1<sp[1]; i1++) {
+                    v_G(i0+tmp_bbox.lb.c[0]-local_bb.lb.c[0],
+                        i1+tmp_bbox.lb.c[1]-local_bb.lb.c[1]) = v_tmp(i0+tmp_bbox.lb.c[0]-src_bbox_tab[i].lb.c[0],
+                                                                        i1+tmp_bbox.lb.c[0]-src_bbox_tab[i].lb.c[1]);
+                }
+            });
+
+            std::cout<<filename<<std::endl;
+        }
+
+        double time_read = timer_read.stop();
+
+        Kokkos::fence();
+
+        double *avg_time_read = nullptr;
+
+        if(rank == 0) {
+            avg_time_read = (double*) malloc(sizeof(double)*nprocs);
+        }
+
+        MPI_Gather(&time_read, 1, MPI_DOUBLE, avg_time_read, 1, MPI_DOUBLE, 0, gcomm);
+
+        if(rank == 0) {
+            for(int i=0; i<nprocs; i++) {
+                avg_read[ts-1] += avg_time_read[i];
+            }
+            avg_read[ts-1] /= nprocs;
+            log << ts << "\t" << avg_read[ts-1] << "\t" << std::endl;
+            total_avg += avg_read[ts-1];
+            free(avg_time_read);
+        }
+
+        Kokkos::parallel_for(sp[0], KOKKOS_LAMBDA(const int i0) {
+            for(int i1=0; i1<sp[1]; i1++) {
+                    std::cout<<v_G(i0, i1)<<"\t";
+                    
+            }
+            std::cout<<std::endl;
+        });
+    }
+
+    free(off);
+    free(lb);
+    free(ub);
+    free(src_bbox_tab);
+    free(avg_read);
+
+    if(rank == 0) {
+        total_avg /= timesteps;
+        log << "Total" << "\t" << total_avg << "\t" << std::endl;
+        //log.close();
+        if(terminate) {
+            std::cout<<"Writer sending kill signal to server."<<std::endl;
+        }
+    }
+
+
+    return 0;
+};
+};
+
+template <class Data_t>
+struct kokkos_run<Data_t, 2, KokkosResilience::HDF5Space> {
+static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
+                    uint64_t* src_sp, uint64_t* offset, int timesteps,
+                    int var_num, bool terminate)
+{
+    int rank, nprocs;
+    MPI_Comm_size(gcomm, &nprocs);
+    MPI_Comm_rank(gcomm, &rank);
+
+
+    using ViewHost_t    = Kokkos::View<Data_t**, Kokkos::HostSpace>;
+    using ViewStaging_t = Kokkos::View<Data_t**, KokkosResilience::HDF5Space>;
+
+    uint64_t* off = (uint64_t*) malloc(2*sizeof(uint64_t));
+    uint64_t* lb = (uint64_t*) malloc(2*sizeof(uint64_t));
+    uint64_t* ub = (uint64_t*) malloc(2*sizeof(uint64_t));
+
+    // get the lb & ub for each rank
+    for(int i=0; i<2; i++) {
+        int tmp = rank;
+        for(int j=0; j<i; j++)
+            tmp /= np[j];
+        off[i] = tmp % np[i] * sp[i];
+        lb[i] = off[i] + offset[i];
+        ub[i] = off[i] + sp[i] - 1 + offset[i];
+    }
+
+    struct bbox local_bb;
+
+    local_bb.num_dims = 2;
+    memcpy(local_bb.lb.c, lb, 2*sizeof(uint64_t));
+    memcpy(local_bb.ub.c, ub, 2*sizeof(uint64_t));
+
+    struct bbox* src_bbox_tab = (struct bbox*) malloc(src_np[0]*src_np[1]*sizeof(struct bbox));
+    int iter[2];
+    for(iter[0]=0; iter[0]<src_np[0]; iter[0]++) {
+        for(iter[1]=0; iter[1]<src_np[1]; iter[1]++) {
+                src_bbox_tab[iter[0]*src_np[1]+iter[1]].num_dims = 2;
+                for(int d=0; d<2; d++) {
+                    src_bbox_tab[iter[0]*src_np[1]+iter[1]].lb.c[d] = iter[d]*src_sp[d];
+                    src_bbox_tab[iter[0]*src_np[1]+iter[1]].ub.c[d] = (iter[d]+1)*src_sp[d]-1;
+                }
+                std::cout<<"lb: {"<<src_bbox_tab[iter[0]*src_np[1]+iter[1]].lb.c[0]<<", "
+                        <<src_bbox_tab[iter[0]*src_np[1]+iter[1]].lb.c[1]<<"}\n"
+                        <<"ub: {"<<src_bbox_tab[iter[0]*src_np[1]+iter[1]].ub.c[0]<<", "
+                        <<src_bbox_tab[iter[0]*src_np[1]+iter[1]].ub.c[1]<<"}"<<std::endl;
+        }
+    }
+    
+
+    
+    int open_num = 0;
+    for(int i=0; i<src_np[0]*src_np[1]; i++) {
+        if(bbox_does_intersect(&local_bb, &src_bbox_tab[i])) {
+            open_num++;
+        }
+    }
+
+    std::vector<std::string> open_tab;
+
+    open_tab.resize(open_num);
+    for(int i=0; i<src_np[0]*src_np[1]; i++) {
+        if(bbox_does_intersect(&local_bb, &src_bbox_tab[i])) {
+            std::string tmp = "StagingView_2D_" + std::to_string(src_bbox_tab[i].lb.c[0]) + "_"
+                                + std::to_string(src_bbox_tab[i].lb.c[1]) + "_"
+                                + std::to_string(src_bbox_tab[i].ub.c[0]) + "_" + std::to_string(src_bbox_tab[i].ub.c[1]);
+            open_tab[i] = tmp;
+        }
+    }
+
+
+
+
+    ViewHost_t v_G("GetView", sp[0], sp[1]);
+    ViewHost_t v_tmp("TmpView", sp[0], sp[1]);
+
+
+    std::ofstream log;
+    double* avg_read = nullptr;
+    double total_avg = 0;
+
+    if(rank == 0) {
+        avg_read = (double*) malloc(sizeof(double)*timesteps);
+        log.open("test_reader.log", std::ofstream::out | std::ofstream::trunc);
+        log << "step\tread_gs" << std::endl;
+    }
+
+    for(int ts=1; ts<=timesteps; ts++) {
+
+        Timer timer_read;
+
+        timer_read.start();
+
+        for(int i=0; i<open_tab.size(); i++) {
+            std::string filename = open_tab[i] + "_t" + std::to_string(ts) + ".hdf";
+
+            ViewStaging_t v_S(filename, src_sp[0], src_sp[1]);
+
+            Kokkos::deep_copy(v_tmp, v_S);
+
+            struct bbox tmp_bbox;
+
+            bbox_intersect(&local_bb, &src_bbox_tab[i], &tmp_bbox);
+            
+
+            Kokkos::parallel_for(sp[0], KOKKOS_LAMBDA(const int i0) {
+                for(int i1=0; i1<sp[1]; i1++) {
+                    v_G(i0+tmp_bbox.lb.c[0]-local_bb.lb.c[0],
+                        i1+tmp_bbox.lb.c[1]-local_bb.lb.c[1]) = v_tmp(i0+tmp_bbox.lb.c[0]-src_bbox_tab[i].lb.c[0],
+                                                                        i1+tmp_bbox.lb.c[0]-src_bbox_tab[i].lb.c[1]);
+                }
+            });
+        }
+
+        double time_read = timer_read.stop();
+
+        Kokkos::fence();
+
+        double *avg_time_read = nullptr;
+
+        if(rank == 0) {
+            avg_time_read = (double*) malloc(sizeof(double)*nprocs);
+        }
+
+        MPI_Gather(&time_read, 1, MPI_DOUBLE, avg_time_read, 1, MPI_DOUBLE, 0, gcomm);
+
+        if(rank == 0) {
+            for(int i=0; i<nprocs; i++) {
+                avg_read[ts-1] += avg_time_read[i];
+            }
+            avg_read[ts-1] /= nprocs;
+            log << ts << "\t" << avg_read[ts-1] << "\t" << std::endl;
+            total_avg += avg_read[ts-1];
+            free(avg_time_read);
+        }
+
+        Kokkos::parallel_for(sp[0], KOKKOS_LAMBDA(const int i0) {
+            for(int i1=0; i1<sp[1]; i1++) {
+                    std::cout<<v_G(i0, i1)<<"\t";
+                    
+            }
+            std::cout<<std::endl;
+        });
+    }
+
+    free(off);
+    free(lb);
+    free(ub);
+    free(src_bbox_tab);
+    free(avg_read);
+
+    if(rank == 0) {
+        total_avg /= timesteps;
+        log << "Total" << "\t" << total_avg << "\t" << std::endl;
+        log.close();
+        if(terminate) {
+            std::cout<<"Writer sending kill signal to server."<<std::endl;
+        }
+    }
+
+
+    return 0;
+};
+};
+
+template <class Data_t>
 struct kokkos_run<Data_t, 3, KokkosResilience::StdFileSpace> {
 static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
                     uint64_t* src_sp, uint64_t* offset, int timesteps,
--- tests/evaluation/test_writer.hpp
+++ tests/evaluation/test_writer.hpp
@@ -15,6 +15,216 @@ struct kokkos_run {
 };
 
 template <class Data_t>
+struct kokkos_run<Data_t, 2, KokkosResilience::StdFileSpace> {
+static int put_run (MPI_Comm gcomm, int* np, uint64_t* sp, int timesteps, int var_num, bool terminate)
+{
+    int rank, nprocs;
+    MPI_Comm_size(gcomm, &nprocs);
+    MPI_Comm_rank(gcomm, &rank);
+
+
+    using ViewHost_t    = Kokkos::View<Data_t**, Kokkos::HostSpace>;
+    using ViewStaging_t = Kokkos::View<Data_t**, KokkosResilience::StdFileSpace>;
+
+    uint64_t* off = (uint64_t*) malloc(2*sizeof(uint64_t));
+    uint64_t* lb = (uint64_t*) malloc(2*sizeof(uint64_t));
+    uint64_t* ub = (uint64_t*) malloc(2*sizeof(uint64_t));
+
+    // get the lb & ub for each rank
+    for(int i=0; i<2; i++) {
+        int tmp = rank;
+        for(int j=0; j<i; j++)
+            tmp /= np[j];
+        off[i] = tmp % np[i] * sp[i];
+        lb[i] = off[i];
+        ub[i] = off[i] + sp[i] - 1;
+    }
+
+    ViewHost_t v_P("PutView", sp[0], sp[1]);
+    //std::string filename = ""
+    //ViewStaging_t v_S("StagingView_3D", sp[0], sp[1]);
+
+    std::ofstream log;
+    double* avg_write = nullptr;
+    double total_avg = 0;
+
+    if(rank == 0) {
+        avg_write = (double*) malloc(sizeof(double)*timesteps);
+        log.open("test_writer.log", std::ofstream::out | std::ofstream::trunc);
+        log << "step\twrite_gs" << std::endl;
+    }
+
+    for(int ts=1; ts<=timesteps; ts++) {
+
+        Kokkos::parallel_for(sp[0], KOKKOS_LAMBDA(const int i0) {
+            for(int i1=0; i1<sp[1]; i1++) {
+                v_P(i0,i1) = i0*sp[1]+i1 + 0.01*ts;
+                std::cout<<v_P(i0,i1)<<"\t";
+            }
+            std::cout<<std::endl;
+        });
+
+        std::string filename = "StagingView_2D_" + std::to_string(lb[0]) + "_"
+                                + std::to_string(lb[1]) + "_" + std::to_string(lb[2]) + "_"
+                                + std::to_string(ub[0]) + "_" + std::to_string(ub[1]) + "_"
+                                + std::to_string(ub[2]) + "_t" + std::to_string(ts) + ".bin";
+        
+        ViewStaging_t v_S(filename, sp[0], sp[1]);
+
+        Timer timer_write;
+
+        timer_write.start();
+
+        Kokkos::deep_copy(v_S, v_P);
+
+        double time_write = timer_write.stop();
+
+        Kokkos::fence();
+
+        double *avg_time_write = nullptr;
+
+        if(rank == 0) {
+            avg_time_write = (double*) malloc(sizeof(double)*nprocs);
+        }
+
+        MPI_Gather(&time_write, 1, MPI_DOUBLE, avg_time_write, 1, MPI_DOUBLE, 0, gcomm);
+
+        if(rank == 0) {
+            for(int i=0; i<nprocs; i++) {
+                avg_write[ts-1] += avg_time_write[i];
+            }
+            avg_write[ts-1] /= nprocs;
+            log << ts << "\t" << avg_write[ts-1] << "\t" << std::endl;
+            total_avg += avg_write[ts-1];
+            free(avg_time_write);
+        }
+    }
+
+    free(off);
+    free(lb);
+    free(ub);
+    free(avg_write);
+
+    if(rank == 0) {
+        total_avg /= timesteps;
+        log << "Total" << "\t" << total_avg << "\t" << std::endl;
+        log.close();
+        if(terminate) {
+            std::cout<<"Writer sending kill signal to server."<<std::endl;
+        }
+    }
+
+
+    return 0;
+};
+};
+
+template <class Data_t>
+struct kokkos_run<Data_t, 2, KokkosResilience::HDF5Space> {
+static int put_run (MPI_Comm gcomm, int* np, uint64_t* sp, int timesteps, int var_num, bool terminate)
+{
+    int rank, nprocs;
+    MPI_Comm_size(gcomm, &nprocs);
+    MPI_Comm_rank(gcomm, &rank);
+
+
+    using ViewHost_t    = Kokkos::View<Data_t**, Kokkos::HostSpace>;
+    using ViewStaging_t = Kokkos::View<Data_t**, KokkosResilience::HDF5Space>;
+
+    uint64_t* off = (uint64_t*) malloc(2*sizeof(uint64_t));
+    uint64_t* lb = (uint64_t*) malloc(2*sizeof(uint64_t));
+    uint64_t* ub = (uint64_t*) malloc(2*sizeof(uint64_t));
+
+    // get the lb & ub for each rank
+    for(int i=0; i<2; i++) {
+        int tmp = rank;
+        for(int j=0; j<i; j++)
+            tmp /= np[j];
+        off[i] = tmp % np[i] * sp[i];
+        lb[i] = off[i];
+        ub[i] = off[i] + sp[i] - 1;
+    }
+
+    ViewHost_t v_P("PutView", sp[0], sp[1]);
+    //std::string filename = ""
+    //ViewStaging_t v_S("StagingView_3D", sp[0], sp[1], sp[2]);
+
+    std::ofstream log;
+    double* avg_write = nullptr;
+    double total_avg = 0;
+
+    if(rank == 0) {
+        avg_write = (double*) malloc(sizeof(double)*timesteps);
+        log.open("test_writer.log", std::ofstream::out | std::ofstream::trunc);
+        log << "step\twrite_gs" << std::endl;
+    }
+
+    for(int ts=1; ts<=timesteps; ts++) {
+
+        Kokkos::parallel_for(sp[0], KOKKOS_LAMBDA(const int i0) {
+            for(int i1=0; i1<sp[1]; i1++) {
+                v_P(i0,i1) = i0*sp[1]+i1 + 0.01*ts;
+                std::cout<<v_P(i0,i1)<<"\t";
+            }
+            std::cout<<std::endl;
+        });
+
+        std::string filename = "StagingView_2D_" + std::to_string(lb[0]) + "_"
+                                + std::to_string(lb[1]) + "_" + std::to_string(lb[2]) + "_"
+                                + std::to_string(ub[0]) + "_" + std::to_string(ub[1]) + "_"
+                                + std::to_string(ub[2]) + "_t" + std::to_string(ts) + ".hdf";
+        
+        ViewStaging_t v_S(filename, sp[0], sp[1]);
+
+        Timer timer_write;
+
+        timer_write.start();
+
+        Kokkos::deep_copy(v_S, v_P);
+
+        double time_write = timer_write.stop();
+
+        Kokkos::fence();
+
+        double *avg_time_write = nullptr;
+
+        if(rank == 0) {
+            avg_time_write = (double*) malloc(sizeof(double)*nprocs);
+        }
+
+        MPI_Gather(&time_write, 1, MPI_DOUBLE, avg_time_write, 1, MPI_DOUBLE, 0, gcomm);
+
+        if(rank == 0) {
+            for(int i=0; i<nprocs; i++) {
+                avg_write[ts-1] += avg_time_write[i];
+            }
+            avg_write[ts-1] /= nprocs;
+            log << ts << "\t" << avg_write[ts-1] << "\t" << std::endl;
+            total_avg += avg_write[ts-1];
+            free(avg_time_write);
+        }
+    }
+
+    free(off);
+    free(lb);
+    free(ub);
+    free(avg_write);
+
+    if(rank == 0) {
+        total_avg /= timesteps;
+        log << "Total" << "\t" << total_avg << "\t" << std::endl;
+        log.close();
+        if(terminate) {
+            std::cout<<"Writer sending kill signal to server."<<std::endl;
+        }
+    }
+
+
+    return 0;
+};
+};
+
+template <class Data_t>
 struct kokkos_run<Data_t, 3, KokkosResilience::StdFileSpace> {
 static int put_run (MPI_Comm gcomm, int* np, uint64_t* sp, int timesteps, int var_num, bool terminate)
 {
