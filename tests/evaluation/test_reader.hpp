c8c6621b84ca1a1930a24aea77efa38cf31a790b
Zhang690683220 <zhangbo_china@live.cn>
2021-05-05 20:29:16 -0400

    debug

--- tests/evaluation/test_reader.hpp
+++ tests/evaluation/test_reader.hpp
@@ -166,7 +166,7 @@ static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
         avg_read = (double*) malloc(sizeof(double)*timesteps);
         log.open("test_reader.log", std::ofstream::out | std::ofstream::trunc);
         log << "step\tread_gs" << std::endl;
-        std::cout << "step\tread_gs" << std::endl;
+        log << "step\tread_gs" << std::endl;
     }
 
     for(int ts=1; ts<=timesteps; ts++) {
@@ -181,21 +181,24 @@ static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
             ViewStaging_t v_S(filename, src_sp[0], src_sp[1]);
 
             Kokkos::deep_copy(v_tmp, v_S);
-/*
+
+            if(rank == 0) {
             std::cout<<"**********"<<i<<"***********"<<std::endl;
 
-            Kokkos::parallel_for(src_sp[0], KOKKOS_LAMBDA(const int i0) {
-            for(int i1=0; i1<src_sp[1]; i1++) {
+            for(int i0=0; i0<src_sp[0]; i0++) {
+                for(int i1=0; i1<src_sp[1]; i1++) {
                     std::cout<<v_tmp(i0, i1)<<"\t";
                     
+                }
+                std::cout<<std::endl;
             }
-            std::cout<<std::endl;
-            });
-*/
+            }
+
             struct bbox tmp_bbox;
 
             bbox_intersect(&local_bb, &src_bbox_tab[i], &tmp_bbox);
-/*
+
+            if(rank == 0) {
             std::cout<<"tmp_bbox: lb = {"<<tmp_bbox.lb.c[0]<<", "<<tmp_bbox.lb.c[1]<<"}\n ub = {"
                     <<tmp_bbox.ub.c[0]<<", "<<tmp_bbox.ub.c[1]<<"}"<<std::endl;
 
@@ -204,20 +207,20 @@ static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
 
             std::cout<<"src_bbox: lb = {"<<src_bbox_tab[i].lb.c[0]<<", "<<src_bbox_tab[i].lb.c[1]<<"}\n ub = {"
                     <<src_bbox_tab[i].ub.c[0]<<", "<<src_bbox_tab[i].ub.c[1]<<"}"<<std::endl;
-*/            
+            }
 
-            Kokkos::parallel_for(src_sp[0], KOKKOS_LAMBDA(const int i0) {
+            for(int i0=0; i0<src_sp[0]; i0++) {
                 for(int i1=0; i1<src_sp[1]; i1++) {
-/*
+                    if(rank == 0) {
                     std::cout<<"v_G("<<i0+tmp_bbox.lb.c[0]-local_bb.lb.c[0]<<", "<<i1+tmp_bbox.lb.c[1]-local_bb.lb.c[1]
                             <<") = v_tmp("<<i0+tmp_bbox.lb.c[0]-src_bbox_tab[i].lb.c[0]<<", "<<i1+tmp_bbox.lb.c[1]-src_bbox_tab[i].lb.c[1]
                             <<") = "<< v_tmp(i0+tmp_bbox.lb.c[0]-src_bbox_tab[i].lb.c[0],i1+tmp_bbox.lb.c[1]-src_bbox_tab[i].lb.c[1])<<std::endl;
-*/                    
+                    }
                     v_G(i0+tmp_bbox.lb.c[0]-local_bb.lb.c[0],
                         i1+tmp_bbox.lb.c[1]-local_bb.lb.c[1]) = v_tmp(i0+tmp_bbox.lb.c[0]-src_bbox_tab[i].lb.c[0],
                                                                         i1+tmp_bbox.lb.c[1]-src_bbox_tab[i].lb.c[1]);
                 }
-            });
+            }
 
             //std::cout<<filename<<std::endl;
         }
@@ -239,8 +242,8 @@ static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
                 avg_read[ts-1] += avg_time_read[i];
             }
             avg_read[ts-1] /= nprocs;
-            //log << ts << "\t" << avg_read[ts-1] << "\t" << std::endl;
-            std::cout << ts << "\t" << avg_read[ts-1] << "\t" << std::endl;
+            log << ts << "\t" << avg_read[ts-1] << "\t" << std::endl;
+            log << ts << "\t" << avg_read[ts-1] << "\t" << std::endl;
             total_avg += avg_read[ts-1];
             free(avg_time_read);
         }
@@ -263,7 +266,7 @@ static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
     if(rank == 0) {
         total_avg /= timesteps;
         log << "Total" << "\t" << total_avg << "\t" << std::endl;
-        std::cout<< "Total" << "\t" << total_avg << "\t" << std::endl;
+        log<< "Total" << "\t" << total_avg << "\t" << std::endl;
         log.close();
         if(terminate) {
             std::cout<<"Writer sending kill signal to server."<<std::endl;
@@ -552,8 +555,6 @@ static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
                     }
                 }
             });
-
-            std::cout<<filename<<std::endl;
         }
 
         double time_read = timer_read.stop();
