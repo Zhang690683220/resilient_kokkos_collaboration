d37dd1a869324c00b0eaade6ec7ec519423ac383
Zhang690683220 <zhangbo_china@live.cn>
2021-05-05 21:44:22 -0400

    debug

--- tests/evaluation/test_reader.hpp
+++ tests/evaluation/test_reader.hpp
@@ -149,9 +149,6 @@ static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
         }
     }
 
-
-/*
-
     ViewHost_t v_G("GetView", sp[0], sp[1]);
     ViewHost_t v_tmp("TmpView", src_sp[0], src_sp[1]);
 
@@ -253,7 +250,7 @@ static int get_run (MPI_Comm gcomm, int* np, uint64_t* sp, int* src_np,
         }
 
     }
-*/
+
     free(off);
     free(lb);
     free(ub);
